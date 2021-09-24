60457b223b54195f0dc82b3f37791b12314f59c6
hantao <937626747@qq.com>
2021-09-24 17:07:03 +0800

    强制用户认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // 必须验证邮箱
+            \App\Http\Middleware\EnsureEmailIsVerified::class,
         ],
 
         'api' => [
