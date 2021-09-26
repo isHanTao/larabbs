bb41e37170fc0c4196f1c16adfb9e38b44a8cd02
hantao <937626747@qq.com>
2021-09-26 15:12:08 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,18 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
     public function creating(Reply $reply)
     {
-        //
+        $content = clean($reply->content, 'user_topic_body');
+        if ($content == ''){
+            $content = 'Xss内容被屏蔽';
+        }
+        $reply->content = $content;
     }
 
     public function updating(Reply $reply)
