ad50a6b28415d4af8520406ca1923c81598e8528
hantao <937626747@qq.com>
2021-09-27 10:56:46 +0800

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Topic $topic, User $user,  Request $request)
+    public function show(Category $category, Topic $topic, User $user,  Request $request, Link $link)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
@@ -17,7 +18,8 @@ class CategoriesController extends Controller
             ->with('user', 'category')   // 预加载防止 N+1 问题
             ->paginate(20);
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category','active_users'));
+        return view('topics.index', compact('topics', 'category','active_users','links'));
     }
 }
