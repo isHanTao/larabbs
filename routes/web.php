db275e26759a5f08e3e7914e0c151cf72befd48f
hantao <937626747@qq.com>
2021-09-27 10:26:20 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 没有权限拒绝访问
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
