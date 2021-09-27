b05991a73b01b11325ba965f3573d0e83ca04c65
hantao <937626747@qq.com>
2021-09-27 10:42:40 +0800

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Topic $topic, Request $request)
+    public function show(Category $category, Topic $topic, User $user,  Request $request)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
             ->where('category_id', $category->id)
             ->with('user', 'category')   // 预加载防止 N+1 问题
             ->paginate(20);
+        $active_users = $user->getActiveUsers();
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category'));
+        return view('topics.index', compact('topics', 'category','active_users'));
     }
 }
