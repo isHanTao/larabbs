c4a64638c12aac59ed651453245337f8f0ef0b71
hantao <937626747@qq.com>
2021-09-30 16:29:41 +0800

    docker

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 用于启动性能采集
+# nohup tideways-daemon &
+
+# 执行migration
+cd /var/www/larabbs artisan migrate --force
+if [ ! -f "public/storage" ] ; then php artisan storage:link; fi
+
+# 下面这2个被注释的命令有助于提高性能，但是可能导致应用不可用，根据需要自己启动
+php artisan optimize
+php artisan api:cache
+if [ $? -eq 0 ] ; then
+    # 启动php-fpm
+    php-fpmelse
+   exit 1
+fi
